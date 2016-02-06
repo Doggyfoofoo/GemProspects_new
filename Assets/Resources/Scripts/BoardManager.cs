71a8040f347604e9a715f05df2ace2bc751f34e2
Doggyfoofoo <hyyaya@gmail.com>
2016-02-05 22:14:11 -0800

    added gem class and mineral deposit class

--- Assets/Resources/Scripts/BoardManager.cs
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -17,6 +17,62 @@ public class BoardManager : MonoBehaviour {
 
 	private GameObject[,] boardTiles; 
 
+	enum Gems {emerald, ruby, sapphire, amethyst, citrine, opal, topaz, morganite}; 
+
+	public class MineralDeposit
+	{
+		int GemType = -1;
+		bool hasMine = false; 
+		bool isOperational = false; //being mined by miners
+
+		public MineralDeposit(){
+			GemType = (int) Random.Range(0, System.Enum.GetNames(typeof(Gems)).Length);
+		}
+
+		public void BuildMine(){
+			hasMine = true; 
+		}
+
+		public bool OpenMine(){
+			if (hasMine == false) {
+				return false; 
+			} else {
+				isOperational = true; 
+				return true;
+			}
+		}
+
+		public bool CloseMine(){
+			if (isOperational == false) {
+				return false; 
+			} else {
+				isOperational = true; 
+				return true; 
+			}
+		}
+	}
+
+	public class Gem
+	{
+		private float gemSize = 0; // in Carats
+		private float gemValue = 0; 
+
+		public Gem(){
+			gemSize = 1/Random.Range(.1f, 20f);
+			gemValue = (float) (gemSize * 1000 + System.Math.Pow(4,gemSize)); 
+		}
+
+		public float GetGemSize(){
+			return (float) System.Math.Round (gemSize, 2); 
+		}
+
+		public float GetGemValue(){
+			return (float) System.Math.Round (gemValue, 2);
+		}
+	}
+
+
+
 
 	public int  GetBoardSize(){
 		return boardSize;
